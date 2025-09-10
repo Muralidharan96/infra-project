6bff4eb5e9bef09212b5d8a2d39b8d93815e1d64
Muralidharan96 <Muralidharanfire@gmail.com>
2025-09-10 18:16:52 +0530

    Added dev env terraform config

---
+++ environments/dev/main.tf
@@ -0,0 +1,16 @@
+module "vpc"{
+    source   = "../../modules/vpc"
+    vcp_cid  ="10.0.0.0/16"
+    env      = "dev"
+    subnet_cidr = "10.0.1.0/24"
+    az       = "ap-south-1a"
+}
+
+module "ec2"{
+    source   ="../../modules/ec2"
+    ami_id   ="ami-02d26659fd82cf299"
+    intsance_type ="t2.micro"
+    subnet_id = module.vpc.subnet_id
+    ec2_count = 3
+    env      = "dev"
+}
